95fc501a18715e40d4122a148c541899fdc88f89
Jeanikt <jeanikkt@gmail.com>
2024-08-09 17:19:47 -0300

    updated page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/once-ui/styles/index.scss";
 import "@/once-ui/tokens/index.scss";
 
+import { Metadata } from "next";
 import { Flex } from '@/once-ui/components'
 import classNames from 'classnames';
 import { Inter } from 'next/font/google'
@@ -46,39 +47,45 @@ const code = Source_Code_Pro({
 	display: 'swap',
 });
 
+export const metadata: Metadata = {
+  title: "Jeazy | Fullstack Developer",
+};
+
 export default function RootLayout({
   	children,
 }: Readonly<{
   	children: React.ReactNode;
 }>) {
 	return (
-    <Flex
-      as="html"
-      lang="en"
-      fillHeight
-      background="page"
-      data-theme="dark"
-      data-brand="cyan"
-      data-accent="magenta"
-      data-neutral="gray"
-      data-border="conservative"
-      data-solid="contrast"
-      data-solid-style="flat"
-      data-surface="translucent"
-      data-transition="all"
-      className={classNames(
-        primary.variable,
-        secondary ? secondary.variable : "",
-        tertiary ? tertiary.variable : "",
-        code.variable,
-        "root"
-      )}
-    >
-      <Flex as="body" fillWidth fillHeight margin="0" padding="0">
-        <Flex flex={1} direction="column">
-          {children}
+    <>
+      <Flex
+        as="html"
+        lang="en"
+        fillHeight
+        background="page"
+        data-theme="dark"
+        data-brand="cyan"
+        data-accent="magenta"
+        data-neutral="gray"
+        data-border="conservative"
+        data-solid="contrast"
+        data-solid-style="flat"
+        data-surface="translucent"
+        data-transition="all"
+        className={classNames(
+          primary.variable,
+          secondary ? secondary.variable : "",
+          tertiary ? tertiary.variable : "",
+          code.variable,
+          "root"
+        )}
+      >
+        <Flex as="body" fillWidth fillHeight margin="0" padding="0">
+          <Flex flex={1} direction="column">
+            {children}
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,126 +1,397 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import {
+  Heading,
+  Text,
+  Flex,
+  Button,
+  Grid,
+  Icon,
+  InlineCode,
+} from "@/once-ui/components";
+import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import Link from "next/link";
 
-import { Heading, Text, Flex, Button, Grid, Icon, InlineCode, Logo } from '@/once-ui/components';
-import Link from 'next/link';
+const textAnimation = `
+  @keyframes gradientAnimation {
+    0% { background-position: 0% 50%; }
+    50% { background-position: 100% 50%; }
+    100% { background-position: 0% 50%; }
+  }
+  
+  .gradient-text {
+    background: linear-gradient(45deg, #00bcd4, #8e2de2); /* Azul ciano a roxo */
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    animation: gradientAnimation 3s ease infinite;
+    font-weight: bold;
+  }
+`;
+
+interface Repo {
+  id: number;
+  name: string;
+  description: string | null;
+  html_url: string;
+}
+
+interface GitHubUser {
+  avatar_url: string;
+  name: string;
+}
+
+interface WorkExperience {
+  id: number;
+  company: string;
+  role: string;
+  period: string;
+  technologies: string[];
+  description: string;
+}
+
+const truncateDescription = (
+  description: string | null,
+  maxLength: number = 65
+): string => {
+  if (!description) return "No description available";
+  return description.length > maxLength
+    ? `${description.substring(0, maxLength)}...`
+    : description;
+};
 
 export default function Home() {
-	const links = [
-		{
-			href: "https://once-ui.com/docs/theming",
-			title: "Themes",
-			description: "Style your app in minutes.",
-		},
-		{
-			href: "https://once-ui.com/docs/flexComponent",
-			title: "Layout",
-			description: "Build responsive layouts.",
-		},
-		{
-			href: "https://once-ui.com/docs/typography",
-			title: "Typography",
-			description: "Scale text automatically.",
-		},
-	];
+  const [repos, setRepos] = useState<Repo[]>([]);
+  const [user, setUser] = useState<GitHubUser | null>(null);
+  const [workExperience, setWorkExperience] = useState<WorkExperience[]>([]);
 
-	return (
-		<Flex style={{ backgroundImage: 'radial-gradient(ellipse at top left, var(--brand-background-strong) 0%, rgba(0,0,0,0) 50%)' }}
-			fillWidth paddingTop="l" paddingX="l"
-			direction="column" alignItems="center" flex={1}>
-			<Flex
-				as="section" overflow="hidden"
-				fillWidth minHeight="0" maxWidth={64}
-				direction="column" alignItems="center" flex={1}>
-				<Flex
-					as="main"
-					direction="column" justifyContent="center"
-					fillWidth fillHeight padding="l" gap="l">
-					<Flex
-						mobileDirection="column"
-						fillWidth gap="24">
-						<Flex
-							position="relative"
-							fillWidth paddingTop="56" paddingX="xl">
-							<Logo size="xl" icon={false} style={{zIndex: '1'}}/>
-						</Flex>
-						<Flex
-							position="relative"
-							fillWidth gap="24" marginBottom="104"
-							direction="column">
-							<InlineCode className="shadow-m" style={{width: 'fit-content', padding: 'var(--static-space-8) var(--static-space-16)'}}>
-								Start by editing <span className="brand-on-background-medium">app/page.tsx</span>
-							</InlineCode>
-							<Heading
-								variant="display-strong-s">
-								Comprehensive.<br/> Responsive.<br/>Accessible.
-							</Heading>
-							<Button
-								href="https://once-ui.com/docs"
-								suffixIcon="chevronRight"
-								variant="secondary">
-								Read docs
-							</Button>
-						</Flex>
-					</Flex>
-					<Grid
-						radius="l"
-						border="neutral-medium"
-						borderStyle="solid-1"
-						columns="repeat(3, 1fr)"
-						tabletColumns="1col"
-						mobileColumns="1col"
-						fillWidth>
-						{links.map((link) => (
-							<Link
-								target="_blank"
-								style={{ padding: 'var(--responsive-space-l)' }}
-								key={link.href}
-								href={link.href}>
-								<Flex
-									fillWidth paddingY="8" gap="8"
-									direction="column">
-									<Flex
-										fillWidth gap="12"
-										alignItems="center">
-										<Text
-											variant="body-strong-m" onBackground="neutral-strong">
-											{link.title}
-										</Text>
-										<Icon size="s" name="arrowUpRight" />
-									</Flex>
-									<Text
-										variant="body-default-s" onBackground="neutral-weak">
-										{link.description}
-									</Text>
-								</Flex>
-							</Link>
-						))}
-					</Grid>
-				</Flex>
-			</Flex>
-			<Flex
-				as="footer"
-				fillWidth paddingX="l" paddingY="m"
-				justifyContent="space-between">
-				<Text
-					variant="body-default-s" onBackground="neutral-weak">
-					© 2024 Jeazy, <Link href="https://github.com/once-ui-system/nextjs-starter?tab=MIT-1-ov-file">MIT License</Link>
-				</Text>
-				<Flex
-					gap="12">
-					<Button
-						href="https://github.com/once-ui-system/nextjs-starter"
-						prefixIcon="github" size="s" variant="tertiary">
-						GitHub
-					</Button>
-					<Button
-						href="https://discord.com/invite/5EyAQ4eNdS"
-						prefixIcon="discord" size="s" variant="tertiary">
-						Discord
-					</Button>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+  useEffect(() => {
+    // Fetch GitHub user data
+    fetch("https://api.github.com/users/Jeanikt")
+      .then((response) => response.json())
+      .then((data: GitHubUser) => setUser(data))
+      .catch((error) =>
+        console.error("Error fetching GitHub user data:", error)
+      );
+
+    // Fetch repositories from GitHub API
+    fetch("https://api.github.com/users/Jeanikt/repos")
+      .then((response) => response.json())
+      .then((data: Repo[]) => {
+        // Filter specific repositories
+        const filteredRepos = data.filter((repo) =>
+          ["Flower", "WebCrawler", "Jeazy", "myBazaar"].includes(repo.name)
+        );
+        setRepos(filteredRepos);
+      })
+      .catch((error) => console.error("Error fetching GitHub repos:", error));
+
+    // Set work experience data
+    setWorkExperience([
+      {
+        id: 1,
+        company: "Inovacomm",
+        role: "Software Engineer",
+        period: "Mar 2023 - Present",
+        technologies: [
+          "C#",
+          "Tailwind CSS",
+          "Grafana",
+          "VoIP",
+          "3CX",
+          "React",
+          "Node.js",
+          "TypeScript",
+          "Javascript",
+          "PHP",
+          "Laravel",
+          "SQL",
+          "C#",
+          "UI/UX",
+        ],
+        description:
+          "Provided support for the 3CX VoIP platform, developed Web APIs, and optimized site processing. Built and styled web pages using React.js and Tailwind CSS, and created dashboards with SQL and Grafana. Integrated CRM systems and handled predictive dialers.",
+      },
+    ]);
+  }, []);
+
+  const getCurrentYear = () => new Date().getFullYear();
+
+  const scrollToAboutMe = () => {
+    const aboutMeSection = document.getElementById("about-me");
+    if (aboutMeSection) {
+      aboutMeSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <>
+      <style>{textAnimation}</style>
+      <Flex
+        style={{
+          backgroundImage:
+            "radial-gradient(ellipse at top left, #46015F 0%, #0000 45%)",
+        }}
+        fillWidth
+        paddingTop="l"
+        paddingX="l"
+        direction="column"
+        alignItems="center"
+        flex={1}
+      >
+        <Flex
+          as="section"
+          overflow="hidden"
+          fillWidth
+          minHeight="0"
+          maxWidth={64}
+          direction="column"
+          alignItems="center"
+          flex={1}
+        >
+          <Flex
+            as="main"
+            direction="column"
+            justifyContent="center"
+            fillWidth
+            fillHeight
+            padding="l"
+            gap="l"
+          >
+            <Flex mobileDirection="column" fillWidth gap="24">
+              <Flex
+                position="relative"
+                fillWidth
+                paddingTop="56"
+                paddingX="xl"
+                direction="row"
+                alignItems="flex-start" // Alinha o conteúdo do perfil à esquerda
+                gap="24"
+              >
+                {user && (
+                  <>
+                    <img
+                      src={user.avatar_url}
+                      alt={`${user.name}'s avatar`}
+                      style={{
+                        borderRadius: "50%",
+                        width: "100px",
+                        height: "100px",
+                      }}
+                    />
+                    <Flex direction="column" alignItems="flex-start">
+                      <Heading variant="display-strong-xs" marginTop="s" />
+                      <Heading variant="display-strong-xs">{user.name}</Heading>
+                      <Text variant="body-strong-s" onBackground="neutral-weak">
+                        Creative Developer
+                      </Text>
+                    </Flex>
+                  </>
+                )}
+              </Flex>
+              <Flex
+                position="relative"
+                fillWidth
+                gap="24"
+                marginBottom="104"
+                direction="column"
+              >
+                <InlineCode
+                  className="shadow-m"
+                  style={{
+                    width: "fit-content",
+                    padding: "var(--static-space-8) var(--static-space-16)",
+                  }}
+                >
+                  Start running{" "}
+                  <span className="brand-on-background-medium">
+                    npm run jeazy
+                  </span>
+                </InlineCode>
+                <Heading variant="display-strong-s">
+                  <span>Hi, I'm Jean 😶‍🌫️</span>
+                  <br />
+                  <span className="gradient-text">Fullstack Developer</span>
+                </Heading>
+                <Button
+                  onClick={scrollToAboutMe}
+                  suffixIcon="chevronRight"
+                  variant="secondary"
+                  style={{
+                    backgroundColor: "transparent",
+                    border: "1px solid #FFFFFF", // Adiciona uma borda para destacar o botão
+                  }}
+                >
+                  Read docs
+                </Button>
+              </Flex>
+            </Flex>
+
+            {/* About Me Section */}
+            <Flex
+              id="about-me"
+              as="section"
+              fillWidth
+              direction="column"
+              alignItems="start"
+              gap="l"
+              padding="l"
+            >
+              <Heading variant="display-strong-xs">About Me</Heading>
+              <Text variant="body-default-m">
+                I’m a passionate Fullstack Developer from Brazil, skilled in
+                both front-end and back-end technologies. I blend technical
+                expertise with a strong design sense to build scalable and
+                visually engaging applications. My goal is to craft seamless
+                user experiences and embrace new challenges with enthusiasm. As
+                a coffee enthusiast, I love fueling my creativity with a great
+                cup of coffee.
+              </Text>
+            </Flex>
+
+            {/* Work Experience Section */}
+            <Flex
+              as="section"
+              fillWidth
+              direction="column"
+              alignItems="start"
+              gap="l"
+              padding="l"
+            >
+              <Heading variant="display-strong-xs">Work Experience</Heading>
+              <Grid
+                radius="l"
+                border="neutral-medium"
+                borderStyle="solid-1"
+                
+                tabletColumns="1col"
+                mobileColumns="1col"
+                fillWidth
+              >
+                {workExperience.map((experience) => (
+                  <Flex
+                    key={experience.id}
+                    fillWidth
+                    padding="l"
+                    direction="column"
+                    gap="s"
+                  >
+                    <Heading variant="display-strong-s">
+                      {experience.company}
+                    </Heading>
+                    <Text variant="body-strong-m">{experience.role}</Text>
+                    <Text variant="body-default-s" onBackground="neutral-weak">
+                      {experience.period}
+                    </Text>
+                    <Text variant="body-default-s" onBackground="neutral-weak">
+                      {experience.description}
+                    </Text>
+                    <Text variant="body-default-s" onBackground="neutral-weak">
+                      Technologies: {experience.technologies.join(", ")}
+                    </Text>
+                  </Flex>
+                ))}
+              </Grid>
+            </Flex>
+
+            {/* Work Experience Section */}
+            <Flex
+              as="section"
+              fillWidth
+              direction="column"
+              alignItems="start"
+              gap="l"
+              padding="l"
+            >
+              <Heading variant="display-strong-xs">Side Projects</Heading>
+
+              {/* Side Projects Section */}
+              <Grid
+                radius="l"
+                border="neutral-medium"
+                borderStyle="solid-1"
+                columns="repeat(3, 1fr)"
+                tabletColumns="1col"
+                mobileColumns="1col"
+                fillWidth
+              >
+                {repos.map((repo) => (
+                  <Link
+                    target="_blank"
+                    style={{ padding: "var(--responsive-space-l)" }}
+                    key={repo.id}
+                    href={repo.html_url}
+                  >
+                    <Flex fillWidth paddingY="8" gap="8" direction="column">
+                      <Flex fillWidth gap="12" alignItems="center">
+                        <Text
+                          variant="body-strong-m"
+                          onBackground="neutral-strong"
+                        >
+                          {repo.name}
+                        </Text>
+                        <Icon size="s" name="arrowUpRight" />
+                      </Flex>
+                      <Text
+                        variant="body-default-s"
+                        onBackground="neutral-weak"
+                      >
+                        {truncateDescription(repo.description)}
+                      </Text>
+                    </Flex>
+                  </Link>
+                ))}
+              </Grid>
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex
+          as="footer"
+          fillWidth
+          paddingX="l"
+          paddingY="m"
+          justifyContent="space-between"
+        >
+          <Text variant="body-default-s" onBackground="neutral-weak">
+            © {getCurrentYear()} Jeazy, <a></a>
+            <Link href="https://github.com/Jeanikt/Jeazy/blob/main/LICENSE">
+              MIT License
+            </Link>
+          </Text>
+          <Flex gap="12">
+            <Button
+              href="https://github.com/Jeanikt/Jeazy.git"
+              prefixIcon="github"
+              size="s"
+              variant="tertiary"
+            >
+              GitHub
+            </Button>
+            <Button
+              href="https://www.linkedin.com/in/jeanikt"
+              size="s"
+              variant="tertiary"
+            >
+              <span className="flex items-center">
+                <FaLinkedinIn className="mr-4" />
+                LinkedIn
+              </span>
+            </Button>
+            <Button
+              href="https://www.instagram.com/jewknd/"
+              prefixIcon="gram"
+              size="s"
+              variant="tertiary"
+            >
+              <span className="flex items-center">
+                <FaInstagram className="mr-4" />
+                Instagram
+              </span>
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
